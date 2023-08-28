b19e8c3809e86b0f40078a17cb3c9d7aff948240
Sidhuhurix <sidharth.s@hurix.com>
2023-08-28 18:20:00 +0530

    Color change

---
+++ logindata.js
@@ -0,0 +1,52 @@
+
+// const mongoose = require("mongoose")
+
+// const logindataSchema =new mongoose.Schema({
+//     userid:{
+//         type:Number,
+//         required:true,
+//         trim:true,
+//         maxLength:50
+//     },
+//     username:{
+//         type:String,
+//         required:true,
+//         trim:true,
+//         maxLength:50
+//     },
+//     password:{
+//         type:String,
+//         required:true,
+//         trim:true,
+//         maxLength:50
+//     },
+//     email:{
+//         type:String,
+//         required:true,
+//         trim:true,
+//         maxLength:50
+//     },
+//     accountcreationdate:{
+//         type:Date,
+//         required:true,
+//         trim:true,
+        
+//     },
+//     accountexpiredate:{
+//         type:Date,
+//         required:true,
+//         trim:true,
+//      },
+//      accountstatus:{
+//         type:Number,
+//         required:true,
+//         },
+//         SessionToken:{
+//         type:Number,
+//         required:true,
+//         }
+        
+    
+//      }
+
+// })
